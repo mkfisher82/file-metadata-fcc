2ec289ef6c4720519d91204a532ed7fd0143e8cc
Mark Fisher <mkfisher82@gmail.com>
2018-04-23 21:27:38 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const upload = multer({ dest: './uploads/' });
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'landing.html'));
+});
+
+app.post('/', upload.single('file'), (req, res) => {
+  res.json({ size: req.file.size });
+});
+
+app.listen(3000);
